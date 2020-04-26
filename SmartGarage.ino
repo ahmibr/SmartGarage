d6257b98c5e5f1b66f9404e8f2540af82eb2357f
Ahmed Ibrahim <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2020-04-26 21:18:22 +0200

    Add IR sensor

--- SmartGarage.ino
+++ SmartGarage.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <IRremote.h>
 
 Servo entry_servo;
 
@@ -8,72 +9,122 @@ const int ENTRY_SERVO_CLOSED_VALUE = 90;
 const int ENTRY_SERVO_OPEN_VALUE = 0;
 
 const int PIN_SPEAKER = 10;
+const int PIN_IR = 2;
+const long IR_COMMAND_VALUE = 16580863;
 
-int pushButton = 2;
+IRrecv ir_recv(PIN_IR);
+decode_results ir_result;
 
 bool door_is_open = false;
 unsigned long last_open_time = 0;
 
-void open_door(){
-  entry_servo.write(ENTRY_SERVO_OPEN_VALUE);
-  delay(SERVO_SETUP_TIME);
+void open_door()
+{
+	entry_servo.write(ENTRY_SERVO_OPEN_VALUE);
+	delay(SERVO_SETUP_TIME);
 }
 
-void close_door(){
-  entry_servo.write(ENTRY_SERVO_CLOSED_VALUE);
-  delay(SERVO_SETUP_TIME);
+void close_door()
+{
+	entry_servo.write(ENTRY_SERVO_CLOSED_VALUE);
+	delay(SERVO_SETUP_TIME);
 }
 
-void setup_entry_servo(){
-  entry_servo.attach(PIN_ENTRY_SERVO);
-  entry_servo.write(ENTRY_SERVO_CLOSED_VALUE);
-  door_is_open = false;
-  delay(SERVO_SETUP_TIME);
+void setup_entry_servo()
+{
+	entry_servo.attach(PIN_ENTRY_SERVO);
+	entry_servo.write(ENTRY_SERVO_CLOSED_VALUE);
+	door_is_open = false;
+	delay(SERVO_SETUP_TIME);
 }
 
-const long OPEN_SECONDS_THRESHOLD = 3;
+const long OPEN_DOOR_TIME_THRESHOLD = 10000;
 
-void play_door_alarm(){
-  digitalWrite(PIN_SPEAKER, HIGH);
-  delay(400);
-  digitalWrite(PIN_SPEAKER, LOW);
-  delay(500);
+unsigned long last_alarm_high_time = 0;
+unsigned long last_alarm_low_time = 0;
+bool alarm_signal_high = false;
+const long ALARM_HIGH_SIGNAL_TIME = 200;
+const long ALARM_LOW_SIGNAL_TIME = ALARM_HIGH_SIGNAL_TIME *1.5;
+
+void play_door_alarm()
+{
+	if (alarm_signal_high)
+	{
+		if (millis() - last_alarm_high_time > ALARM_HIGH_SIGNAL_TIME)
+		{
+			digitalWrite(PIN_SPEAKER, LOW);
+			last_alarm_low_time = millis();
+			alarm_signal_high = false;
+		}
+	}
+	else
+	{
+		if (millis() - last_alarm_low_time > ALARM_LOW_SIGNAL_TIME)
+		{
+			digitalWrite(PIN_SPEAKER, HIGH);
+			last_alarm_high_time = millis();
+			alarm_signal_high = true;
+		}
+	}
+
+	delay(10);
 }
 
-bool is_open_overdue(){
-  if(door_is_open && (millis()-last_open_time > 1000*OPEN_SECONDS_THRESHOLD))
-    return true;
-  
-  return false;
+void stop_door_alarm()
+{
+	digitalWrite(PIN_SPEAKER, LOW);
+	if (alarm_signal_high)
+		last_alarm_high_time = 0;
+	alarm_signal_high = false;
+	delay(10);
 }
 
-void setup() {
-    setup_entry_servo();
-	Serial.begin(9600);
-    pinMode(pushButton, INPUT);
-    pinMode(PIN_SPEAKER, OUTPUT);
+bool is_open_overdue()
+{
+	if (door_is_open && (millis() - last_open_time > OPEN_DOOR_TIME_THRESHOLD))
+		return true;
+
+	return false;
 }
 
-void loop() {
-  int buttonState = digitalRead(pushButton);
+void setup()
+{
+  	Serial.begin(9600);
+	setup_entry_servo();
+	pinMode(PIN_SPEAKER, OUTPUT);
+ 	ir_recv.enableIRIn(); 
+}
 
-  if(is_open_overdue()){
-    play_door_alarm();
-  }
-  
-  if (buttonState == 1) {
-      if(door_is_open)
-      {
-        close_door();
-        door_is_open = false;
-      }
-      else
-      {
-        open_door();
-        door_is_open = true;
-        last_open_time = millis();
-      }
-      Serial.println(buttonState);
-  }
-  delay(1);
+void loop()
+{
+	if (is_open_overdue())
+	{
+		play_door_alarm();
+	}
+
+	if (ir_recv.decode(&ir_result))
+	{
+              
+      	Serial.println("Received from IR");
+      	Serial.println(ir_result.value);
+      
+		if (ir_result.value == IR_COMMAND_VALUE)
+		{
+			if (door_is_open)
+			{
+				stop_door_alarm();
+				close_door();
+				door_is_open = false;
+			}
+			else
+			{
+				open_door();
+				door_is_open = true;
+				last_open_time = millis();
+			}
+		}
+		ir_recv.resume();	// Receive the next value
+	}
+
+	delay(10);
 }
